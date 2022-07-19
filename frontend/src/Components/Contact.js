dea89f34c6eaf609f9e3490cfc0038642b88dba5
Thierno Sissoko <thiernosissoko@AdministorsMBP2.attlocal.net>
2022-07-18 23:28:21 -0400

    nav working

--- frontend/src/Components/Contact.js
+++ frontend/src/Components/Contact.js
@@ -1,6 +1,8 @@
 import { Container } from "react-bootstrap";
 import Footer from "./Footer";
 import Header from "./Header";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 const Contact = () => {
   return (
@@ -9,6 +11,26 @@ const Contact = () => {
       <main className="py-3">
         <Container>
           <h1>Welcome to the contact page</h1>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
         </Container>
       </main>
       <Footer />
